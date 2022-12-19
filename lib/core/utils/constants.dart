e9d6b546adc46e1051653d1736b187445fa2356c
shervin <shervin.hz07@gmail.com>
2022-12-19 13:18:31 -0800

    _

--- lib/core/utils/constants.dart
+++
@@ -1,5 +0,0 @@
-class AgoraChatConfig {
-  static const String appKey = "<#Your app key#>";
-  static const String userId = "<#Your created user#>";
-  static const String agoraToken = "<#User Token#>";
-}
